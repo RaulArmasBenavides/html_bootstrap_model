d132ee57ade0840caa4ad00017c1f5c2b702da2d
Raul Armas <you@example.com>
2023-10-13 01:07:47 -0500

    update

--- pages/js/jsEventos.js
+++ pages/js/jsEventos.js
@@ -1,5 +1,7 @@
-﻿$(document).ready(function () {
-
+﻿
+ 
+$(document).ready(function () {
+    console.log("test");
     $(".btn-create").click(function (e) {
         $("#modal").load('/Facturacion/Create');
 
@@ -43,13 +45,12 @@
     $("#Guardar").click(function () {
         alert('Factura registrado con éxito');
     });
-    //$("#Actualizar").click(function () {
-    //    alert('Producto actualizado con éxito');
-    //});
-    //$("#Eliminar").click(function () {
-    //    alert('Producto eliminado con éxito');
-    //});
+ 
+
 });
+
+
+ 
 
 function calcular() {
     var c = $("#Cantidad").val();
@@ -63,3 +64,106 @@ function calcular() {
 }
 
 
+
+function RegistrarDocumentos() 
+{
+    var lugar = "";
+    var listaABuscar = null;
+    try 
+    {
+
+        ControlaError = false;
+        lugar ="ANTES DE CARGA EXCEL";
+        CargaExcel();
+
+        lugar = "ANTES DE DESCARGAR EXCEL";
+        //DescargaExcelResultados(listaABuscar);
+
+        lugar ="LUEGO DE DESCARGAR EXCEL";
+        return true;
+    }
+    catch(e)
+    {
+        ControlErroresScript(e,lugar);
+    }
+}
+
+function DescargaExcelResultados(lista) 
+{
+    var parametros =
+    {
+        ListaABuscar: lista,
+        TipoBusqueda: "VENTAS"
+    };
+    jsShowWindowLoad();
+    $.ajax(
+        {
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            dataType: 'json',
+            url: rutaProyecto + "Home/GeneraExcel",
+            data: JSON.stringify(parametros),
+            success: function (data, status) 
+            {
+                window.location = rutaProyecto + 'Home/Descarga?fileGuid=' + data.FileGuid + '&filename=' + data.FileName;
+                jsRemoveWindowLoad();
+            }
+            , error: ControlErroresAjax
+        }
+        );
+
+}
+    
+
+function Limpiar() 
+{
+    document.getElementById("divDocumentosCargados").innerHTML = "";
+    document.getElementById('lblNombreArchivo').value = "";
+    $("#imgCargando").hide();
+}
+
+
+
+function MostrarNombreArchivo() 
+{
+    var lugar = "";
+    try 
+    {
+        var nombreArchivo = "";
+        var longitudCadena = 0;
+        var longitudPath = 0;
+
+        lugar = "OBTENIENDO NOMBRE ARCHIVO";
+        nombreArchivo = document.getElementById('inpFile').value;
+
+        lugar = "OBTENIENDO LONGITUD CADENA";
+        longitudCadena = nombreArchivo.length;
+        
+        lugar = "OBTENIENDO LONGITUD PATH";
+        longitudPath = nombreArchivo.lastIndexOf("\\");
+
+        lugar = "OBTENIENDO NOMBRE ARCHIVO";
+        nombreArchivo = nombreArchivo.substr(longitudPath + 1, longitudCadena - longitudPath);
+
+        lugar = "ASIGNANDO NOMBRE ARCHIVO";
+        document.getElementById('lblNombreArchivo').value = nombreArchivo;
+    }
+    catch (e) 
+    {
+        ControlErroresScript(e, lugar);
+    }
+
+}
+
+
+function CancelarDetalle() 
+{
+    try 
+    {
+        $("#detalle-panel").fadeOut(1000).addClass("hidden");
+        $("#principal-panel").fadeIn(1000).removeClass("hidden");
+    }
+    catch (e) {
+        ControlaError(e);
+    }
+}
